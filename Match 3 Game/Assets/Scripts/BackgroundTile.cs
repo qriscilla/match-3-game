8657fbbee5e404ebf78d4fffa5a4fd3549aa4d4c
qriscilla <pri.whang@gmail.com>
2020-04-19 16:06:43 -0400

    Started all oover. Completed setting up the board.

--- Match 3 Game/Assets/Scripts/BackgroundTile.cs
+++ Match 3 Game/Assets/Scripts/BackgroundTile.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class BackgroundTile : MonoBehaviour
 {
 
+    public GameObject[] dots;
+
     // Start is called before the first frame update
     void Start()
     {
-        Initialize();
+        Initialize(); // Calling the Initialize() method 
     }
 
     // Update is called once per frame
@@ -19,6 +21,9 @@ public class BackgroundTile : MonoBehaviour
 
     void Initialize()
     {
-
+        int dotToUse = Random.Range(0, dots.Length); // Choose a random number between 0 and however long the dots array is
+        GameObject dot = Instantiate(dots[dotToUse], transform.position, Quaternion.identity);
+        dot.transform.parent = this.transform; // The dot is now a child of the background tile
+        dot.name = this.gameObject.name;
     }
 }
--- Match 3 Game/Assets/Scripts/Board.cs
+++ Match 3 Game/Assets/Scripts/Board.cs
@@ -2,153 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Board : MonoBehaviour {
+public class Board : MonoBehaviour
+{
 
-    public int width; // Width of each cell
-    public int height; // Height of each cell
-    public GameObject tilePrefab;
-    public GameObject[] dots;
-    private BackgroundTile[,] allTiles; // 2D array
-    public GameObject[,] allDots;
+    public int width; // Each cell's width
+    public int height; // Each cell's height
+    public GameObject tilePrefab; // The tile we want to create
+    private BackgroundTile[,] allTiles; // Creating an empty 2D array
 
     // Start is called before the first frame update
     void Start()
     {
         allTiles = new BackgroundTile[width, height];
-        allDots = new GameObject[width, height];
-        SetUp();
+        SetUp(); // Calling the SetUp() method
     }
 
-
-    private void SetUp() {
-        for(int i = 0; i < width; i ++) {
-            for(int j = 0; j < height; j ++) {
+    private void SetUp()
+    {
+        for(int i = 0; i < width; i ++) // i represents the x-coordinate
+        {
+            for(int j = 0; j < height; j ++) // j represents the y-coordinate
+            {
                 Vector2 tempPosition = new Vector2(i, j);
-                GameObject backgroundTile = Instantiate(tilePrefab, tempPosition, Quaternion.identity) as GameObject;
-                backgroundTile.transform.parent = this.transform; // Parent object is now the board object
-                backgroundTile.name = "( " + i + ", " + j + " )";
-                int dotToUse = Random.Range(0, dots.Length);
-
-                int maxIterations = 0;
-                while(MatchesAt(i, j, dots[dotToUse]) && maxIterations < 100) {
-                    dotToUse = Random.Range(0, dots.Length);
-                    maxIterations++;
-                    //Debug.Log(maxIterations);
-                }
-                maxIterations = 0;
-
-                GameObject dot = Instantiate(dots[dotToUse], tempPosition, Quaternion.identity);
-                dot.transform.parent = this.transform; // Dot is now the child of the background tile
-                dot.name = "( " + i + ", " + j + " )";
-                allDots[i, j] = dot;
+                GameObject backgroundTile = Instantiate(tilePrefab, tempPosition, Quaternion.identity) as GameObject; // Instantiate(What you're instantiating, Position you're instantiating that, The rotation of the instantiated object)
+                backgroundTile.transform.parent = this.transform; // Setting the parent to the board object
+                backgroundTile.name = "( " + i + ", " + j + " )"; // Change name of backgroundTile
             }
         }
-    }
-
-    private bool MatchesAt(int column, int row, GameObject piece) {
-        if(column > 1 && row > 1) {
-            if(allDots[column - 1, row].tag == piece.tag && allDots[column - 2, row].tag == piece.tag) {
-                return true;
-            }
-            if(allDots[column, row - 1].tag == piece.tag && allDots[column, row - 2].tag == piece.tag) {
-                return true;
-            }
-        } else if(column <= 1 || row <= 1) {
-            if(row > 1) {
-                if(allDots[column, row - 1].tag == piece.tag && allDots[column, row - 2].tag == piece.tag) {
-                    return true;
-                }
-            }
-            if(column > 1) {
-                if (allDots[column - 1, row].tag == piece.tag && allDots[column - 2, row].tag == piece.tag) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    private void DestroyMatchesAt(int column, int row) {
-        if(allDots[column, row].GetComponent<Dot>().isMatched) {
-            Destroy(allDots[column, row]);
-            allDots[column, row] = null;
-        }
-    }
-
-    public void DestroyMatches() {
-        for(int i = 0; i < width; i ++) {
-            for(int j = 0; j < height; j ++) {
-                if(allDots[i, j] != null) {
-                    DestroyMatchesAt(i, j);
-                }
-            }
-        }
-        StartCoroutine(DecreaseRowCo());
-    }
-
-    private IEnumerator DecreaseRowCo() {
-        int nullCount = 0; // Counting null spaces
-        for(int i = 0; i < width; i ++) {
-            for(int j = 0; j < height; j ++){
-                if(allDots[i, j] == null) { // If there's a null spot(s)
-                    nullCount++; // Increase null count
-                } else if(nullCount > 0) {
-                    allDots[i, j].GetComponent<Dot>().row -= nullCount;
-                    allDots[i, j] = null; // When the next dot fills in, it should maintain its current position
-                }
-            }
-            nullCount = 0; // Reset null count back to zero
-        }
-        yield return new WaitForSeconds(.4f);
-        StartCoroutine(FillBoardCo());
-    }
-
-    private void RefillBoard()
-    {
-        for (int i = 0; i < width; i++)
-        {
-            for (int j = 0; j < height; j++)
-            {
-                if (allDots[i, j] == null)
-                {
-                    Vector2 tempPosition = new Vector2(i, j);
-                    int dotToUse = Random.Range(0, dots.Length);
-                    GameObject piece = Instantiate(dots[dotToUse], tempPosition, Quaternion.identity);
-                    allDots[i, j] = piece;
-                }
-            }
-        }
-    }
-
-    private bool MatchesOnBoard()
-    {
-        for (int i = 0; i < width; i++)
-        {
-            for (int j = 0; j < height; j++)
-            {
-                if (allDots[i, j] != null)
-                {
-                    if (allDots[i, j].GetComponent<Dot>().isMatched)
-                    {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    private IEnumerator FillBoardCo()
-    {
-        RefillBoard();
-        yield return new WaitForSeconds(.5f);
-
-        while (MatchesOnBoard())
-        {
-            yield return new WaitForSeconds(.5f);
-            DestroyMatches();
-        }
-
     }
 
 }
